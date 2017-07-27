ae1cdfcb26804f466345a33e4b4f3b1ac01911a1
igor <igor.sv.n@gmail.com>
2017-07-27 09:35:01 +0300

    Refactor speaker.

--- src/Speaker/Speaker.php
+++ src/Speaker/Speaker.php
@@ -10,6 +10,7 @@
 
 namespace Phestival\Speaker;
 
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 
 /**
@@ -18,23 +19,102 @@ use Symfony\Component\Process\Process;
 class Speaker
 {
     /**
-     * @var string
+     * @var \Symfony\Component\Filesystem\Filesystem
      */
-    private $language;
+    private $filesystem;
 
     /**
-     * @param string $language Language
+     * @var string
      */
-    public function __construct(string $language)
+    private $playCommand;
+
+    /**
+     * @var string
+     */
+    private $synthesizeCommand;
+
+    /**
+     * @var string|null
+     */
+    private $tmpFilePathname;
+
+    /**
+     * @param \Symfony\Component\Filesystem\Filesystem $filesystem        Filesystem
+     * @param string                                   $playCommand       Play command
+     * @param string                                   $synthesizeCommand Speech synthesize command
+     */
+    public function __construct(Filesystem $filesystem, string $playCommand, string $synthesizeCommand)
     {
-        $this->language = $language;
+        $this->filesystem = $filesystem;
+        $this->playCommand = $playCommand;
+        $this->synthesizeCommand = $synthesizeCommand;
+
+        $this->tmpFilePathname = null;
+    }
+
+    public function __destruct()
+    {
+        $this->cleanup();
     }
 
     /**
-     * @param string $text Text
+     * @param string $text Text to speak
      */
     public function speak(string $text)
     {
-        (new Process(sprintf('echo "%s" | festival --tts --language %s', $text, $this->language)))->mustRun();
+        $this->createTmpFile()->synthesize($text)->play()->cleanup();
+    }
+
+    /**
+     * @return self
+     */
+    private function createTmpFile()
+    {
+        $this->tmpFilePathname = $this->filesystem->tempnam(sys_get_temp_dir(), 'phestival');
+
+        return $this;
+    }
+
+    /**
+     * @param string $text Text to speak
+     *
+     * @return self
+     */
+    private function synthesize(string $text)
+    {
+        $command = strtr($this->synthesizeCommand, [
+            '{TEXT}' => $text,
+            '{FILE}' => $this->tmpFilePathname,
+        ]);
+
+        (new Process($command))->mustRun();
+
+        return $this;
+    }
+
+    /**
+     * @return self
+     */
+    private function play()
+    {
+        $command = strtr($this->playCommand, [
+            '{FILE}' => $this->tmpFilePathname,
+        ]);
+
+        (new Process($command))->mustRun();
+
+        return $this;
+    }
+
+    /**
+     * @return self
+     */
+    private function cleanup()
+    {
+        if (!empty($this->tmpFilePathname)) {
+            $this->filesystem->remove($this->tmpFilePathname);
+        }
+
+        return $this;
     }
 }
